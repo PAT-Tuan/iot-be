7cd7608119fc015ce2c3929f095890db569079eb
tuan <tonytuanpat2601@gmail.com>
2024-04-11 11:24:32 +0700

    update

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,36 +1,34 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const { error } = require("console");
 
 let refreshTokens = [];
-console.log("🚀 ~ refreshTokenSSS:", refreshTokens);
+
 const authController = {
     //Register
     registerUser: async (req, res) => {
         try {
             const hashed = await bcrypt.hash(req.body.password, 10);
 
-            //Creat new user
+            // Create new user
             const newUser = new User({
                 email: req.body.email,
                 password: hashed,
             });
 
-            //Save to DB
+            // Save to DB
             const user = await newUser.save();
             res.status(200).json({
                 isSuccess: true,
                 data: user
             });
         } catch (error) {
-            console.log("🚀 ~ registerUser:async ~ error:", error)
-
+            console.log("Error in registerUser:", error);
             res.status(500).json(error);
         }
     },
-    // GENERATE ACCESS TOKEN
+
+    // Generate ACCESS TOKEN
     generateAccessToken: (user) => {
         return jwt.sign(
             {
@@ -41,8 +39,9 @@ const authController = {
             { expiresIn: "35s" }
         );
     },
-    // GENERATE REFRESH TOKEN
-    generaterefreshToken: (user) => {
+
+    // Generate REFRESH TOKEN
+    generateRefreshToken: (user) => {
         return jwt.sign(
             {
                 id: user._id,
@@ -55,58 +54,47 @@ const authController = {
 
     loginUser: async (req, res) => {
         try {
-            const userRepo = new User()
+            const user = await User.findOne({ email: req.body.email });
 
-            const user = await userRepo.collection.findOne({
-                "email": req.body.email
-            })
-
-            if (user == null) {
-                res.status(200).json({
-                    "isSuccess": false,
-                    "errMessage": "User not found",
-                    "data": null
-                })
+            if (!user) {
+                return res.status(404).json({ isSuccess: false, message: "User not found" });
             }
 
-            console.log(user)
-
-            var isMatch = await bcrypt.compare(req.body.password, user.password)
+            const isMatch = await bcrypt.compare(req.body.password, user.password);
 
             if (isMatch) {
                 const token = authController.generateAccessToken(user);
-                const refreshToken = authController.generaterefreshToken(user);
+                const refreshToken = authController.generateRefreshToken(user);
                 
                 // push refreshToken
                 refreshTokens.push(refreshToken);
                 
-                //save cookie
-                res.cookie("refreshToken", refreshToken,{
-                    httpOnly:true,
+                res.cookie("refreshToken", refreshToken, {
+                    httpOnly: true,
                     secure: false,
                     path: "/",
                     sameSite: "strict",
-                })
+                });
 
                 return res.status(200).json({
-                    "isSuccess": true,
-                    "user": user,
-                    "token": token,
+                    isSuccess: true,
+                    user: user,
+                    token: token,
                 });
             } else {
-                res.status(200).json({
-                    "isSuccess": false,
-                    "errMessage": "password incorrect"
-                })
+                return res.status(200).json({
+                    isSuccess: false,
+                    message: "Password incorrect"
+                });
             }
 
         } catch (error) {
-            console.log("🚀 ~ loginUser:async ~ error:", error)
+            console.log("Error in loginUser:", error);
             res.status(500).json(error);
         }
     },
 
-    requestRefreshToken: async(req, res) =>{
+    requestRefreshToken: async (req, res) => {
         try {
             // Lấy refresh token từ cookie
             const refreshToken = req.cookies.refreshToken;
@@ -121,45 +109,27 @@ const authController = {
                 return res.status(403).json("Refresh token is not valid");
             }
             
-            // // Tìm người dùng tương ứng với refreshToken trong cơ sở dữ liệu
-            // const user = await User.findOne({ refreshToken: refreshToken });
+            const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+            const user = await User.findById(decoded.id);
 
-            // // Kiểm tra xem người dùng có tồn tại không
-            // if (!user) {
-            //     return res.status(404).json({ message: "User not found" });
-            // }
-
-            
-            // Xác minh refreshToken và lấy thông tin người dùng
-            jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (error, user) => {
-                if (error) {
-                    console.log(error);
-                    return res.status(500).json({ message: "Server error" });
-                }
-    
-                // Loại bỏ refreshToken đã sử dụng khỏi danh sách
-                refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
-    
-                // Tạo mới accessToken và refreshToken
-                const newAccessToken = authController.generateAccessToken(user);
-                const newRefreshToken = authController.generaterefreshToken(user);
-    
-                // Gửi refreshToken mới trong cookie
-                res.cookie("refreshToken", newRefreshToken, {
-                    httpOnly: true,
-                    secure: false,
-                    path: "/",
-                    sameSite: "strict",
-                });
-    
-                // Trả về accessToken mới
-                return res.status(200).json({ accessToken: newAccessToken });
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            const newRefreshToken = authController.generateRefreshToken(user);
+            // Gửi refreshToken mới trong cookie
+            res.cookie("refreshToken", newRefreshToken, {
+                httpOnly: true,
+                secure: false,
+                path: "/",
+                sameSite: "strict",
             });
+            const newAccessToken = authController.generateAccessToken(user);
+            return res.status(200).json({ accessToken: newAccessToken });
         } catch (error) {
             console.log("Error in requestRefreshToken:", error);
             return res.status(500).json({ message: "Server error" });
         }
     }
-}
+};
 
 module.exports = authController;
